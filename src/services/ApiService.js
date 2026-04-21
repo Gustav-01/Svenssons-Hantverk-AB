93b96be9d257edacf8470ca6d4c13b68c7d81841
Gustav O <ac75309@skola.goteborg.se>
2026-04-21 12:51:23 +0200

    Formating

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,17 +1,17 @@
- class ApiService {
+class ApiService {
 
     async getAllData() {
         try {
             const response = await fetch("https://yrgo-web-services.netlify.app/bookings");
 
             if (!response.ok) {
-            throw new Error(`Problem fetching: ${response.status}`);
+                throw new Error(`Problem fetching: ${response.status}`);
             }
 
             return await response.json();
 
         } catch (error) {
-        console.error("Fetch failed: ", error);
+            console.error("Fetch failed: ", error);
         }
     }
 }
