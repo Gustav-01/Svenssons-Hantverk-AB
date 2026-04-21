64ee3dcac9ae4883f8eddc281879b9165f1e8d34
Gustav O <ac75309@skola.goteborg.se>
2026-04-21 22:07:20 +0200

    Added status component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { BookingService } from './services/BookingService';
+import  StatusComponent from './components/StatusComponent.vue'
 
 const bookingService = new BookingService();
 bookingService.getAllEmployees();
 </script>
 
 <template>
-
+    <StatusComponent></StatusComponent>
 </template>
 
 <style scoped>
