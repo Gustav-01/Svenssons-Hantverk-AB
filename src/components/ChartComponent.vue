dad4b4fc9ee0e73ed127ff6f1be69a4e80378ba9
Gustav O <ac75309@skola.goteborg.se>
2026-04-22 14:35:24 +0200

    Added component directory in src and some basic components

---
+++ src/components/ChartComponent.vue
@@ -0,0 +1,12 @@
+<script setup>
+import RowComponent from './RowComponent.vue';
+
+</script>
+
+<template>
+
+</template>
+
+<style scope>
+
+</style>
---
+++ src/components/RowComponent.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NameColumnComponent from './NameColumnComponent.vue';
+
+</script>
+
+<template>
+
+</template>
+
+<style scope>
+</style>
