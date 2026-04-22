9e8c95a9e78154e981fe290cab4902ca20d027c0
Gustav O <ac75309@skola.goteborg.se>
2026-04-22 17:04:46 +0200

    Added nameColumnComponent to parent components

--- src/components/NameColumnComponent.vue
+++ src/components/NameColumnComponent.vue
@@ -3,6 +3,7 @@ import painter from '../assets/painter.png'
 import carpenter from '../assets/carpenter.png'
 import electrician from '../assets/electrician.png'
 import plumber from '../assets/plumber.png'
+import mason from '../assets/mason.png' 
 
 const props = defineProps({
     name: String,
@@ -13,7 +14,8 @@ const professionImages = {
     Painter: painter,
     Carpenter: carpenter,
     Electrician: electrician,
-    Plumber: plumber
+    Plumber: plumber,
+    Mason: mason
 }
 </script>
 
@@ -21,12 +23,32 @@ const professionImages = {
     <div class="employee-container">
         <span>{{ name }}</span>
         <div class="icons">
-            <img 
-                v-for="profession in professions" 
-                :key="profession"
-                :src="professionImages[profession]" 
-                alt="profession image"
-            >
+            <img v-for="profession in professions" :key="profession" :src="professionImages[profession]"
+                alt="profession image">
         </div>
     </div>
 </template>
+
+<style scoped> 
+
+.employee-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 2px 4px;
+    border: 1px solid #ddd;
+    background-color: #fff;
+    width: 160px;
+    height: 32px;
+}
+
+span {
+    font-size: 13px;
+}
+
+img {
+    width: 20px;
+    height: 20px;
+    object-fit: contain;
+}
+</style>
