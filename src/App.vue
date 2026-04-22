0b4727efca0049c933724547acae064d285c73d3
Gustav O <ac75309@skola.goteborg.se>
2026-04-22 14:29:39 +0200

    Added images

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import NameColumnComponent from './components/NameColumnComponent.vue';
 import StatusComponent from './components/StatusComponent.vue'
 
 </script>
 
 <template>
     <StatusComponent></StatusComponent>
+    <NameColumnComponent></NameColumnComponent>
 </template>
 
 <style scoped>
---
+++ src/components/NameColumnComponent.vue
@@ -0,0 +1,32 @@
+<script setup>
+import painter from '../assets/painter.png'
+</script>
+
+<template>
+    <div class="employee-container">
+        <span>Aisha Abdi</span>
+        <img :src="painter" alt="profession image">
+    </div>
+</template> 
+
+<style scope>
+    .employee-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border: 1px solid #ddd;
+    padding: 4px 8px;
+
+}
+
+span {
+    font-size: 13px;
+}
+
+img {
+    width: 26px;
+    height: 26px;
+    object-fit: contain;
+}
+
+</style>
