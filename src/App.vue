ba16b0c345ea4482f6a1f6bcbf19a0b7629582c2
Gustav O <ac75309@skola.goteborg.se>
2026-04-22 11:19:05 +0200

    BookingService.js removed

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script setup>
-import { BookingService } from './services/BookingService';
 import  StatusComponent from './components/StatusComponent.vue'
 
-const bookingService = new BookingService();
-bookingService.getAllEmployees();
 </script>
 
 <template>
