a1ded761f243dd9a25e4b5079b22e6359738dda5
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-04-27 11:25:45 +0200

    Removing unused methods and rearranging error handling

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,7 @@
 class ApiService {
 
     async getAllData() {
-        try {
+        
             const response = await fetch("https://yrgo-web-services.netlify.app/bookings");
 
             if (!response.ok) {
@@ -9,10 +9,6 @@ class ApiService {
             }
 
             return await response.json();
-
-        } catch (error) {
-            console.error("Fetch failed: ", error);
-        }
     }
 }
 
